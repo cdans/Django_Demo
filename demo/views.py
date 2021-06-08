26d91c76abcc915997d6616a751678bda6616fd4
cdans <ch.dansard@gmail.com>
2021-06-08 22:51:01 +0200

    urls and views

--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
+from .models import Book
 
-# Create your views here.
+class Another(View):
+
+    books = Book.objects.all()
+    output1 = ""
+    for book in books:
+        output1 += f"<p>We have '{book.title}' with ID {book.id} in DB.<p/>"
+
+    output2 = f"<h1>We have {len(books)} books in DB.<h1/>"
+
+    def get(self, request):
+        return HttpResponse(self.output1 + self.output2)
+
+
+def first(request):
+    return HttpResponse('First message from views')
