63030de4833d38ccf21845e61245e7d178fe2658
cdans <ch.dansard@gmail.com>
2021-06-08 21:55:18 +0200

    Create table Book, without date and choices

--- demo/models.py
+++ demo/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
-# Create your models here.
 class Book (models.Model):
-    title = models.CharField(max_length=36)
- 
+     title = models.CharField(max_length=32, blank=False, unique=True, null=True)
+     description = models.TextField(max_length=256, blank=True)
+     price = models.DecimalField(max_digits=16, decimal_places=2, default=0)
+     is_published = models.BooleanField(default=False)
